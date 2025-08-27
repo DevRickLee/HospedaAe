c143411b367ddf7715c383860c2d216efd0afbfd
Wérick Santos <202411240021@ifba.edu.br>
2025-08-26 23:57:48 -0300

    Commit inicial do projeto HospedaAe!

---
+++ src/main/java/br/com/devlee/hospedaae/HospedaAeApplication.java
@@ -0,0 +1,13 @@
+package br.com.devlee.hospedaae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospedaAeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospedaAeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devlee/hospedaae/HospedaAeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devlee.hospedaae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedaAeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
