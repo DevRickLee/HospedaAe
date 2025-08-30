f59e750f881f2f4d466d31ad187fde1cdf148346
thalita <dev.thalita@gmail.com>
2025-08-30 09:12:56 -0300

    feat: mapeia mensagem de boas vindas no contex path /

--- src/main/java/br/com/devlee/hospedaae/HospedaAeApplication.java
+++ src/main/java/br/com/devlee/hospedaae/HospedaAeApplication.java
@@ -2,12 +2,20 @@ package br.com.devlee.hospedaae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HospedaAeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospedaAeApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String inicio(){
+		String msg = "Seja-Bem vindo ao HospedaAe";
+		return msg;
+	}
 }
